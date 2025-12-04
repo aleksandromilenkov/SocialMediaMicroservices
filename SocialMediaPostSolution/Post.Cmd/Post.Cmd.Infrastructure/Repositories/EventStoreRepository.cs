b939e8882bed04deb84c985e2c53190b8ff62678
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2025-12-04 20:22:00 +0100

    implementing Event Repository  and Connecting to MongoDb

---
+++ SocialMediaPostSolution/Post.Cmd/Post.Cmd.Infrastructure/Repositories/EventStoreRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CQRS.Core.Domain;
+using CQRS.Core.Events;
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+using Post.Cmd.Infrastructure.Config;
+
+namespace CQRS.Core.Repositories
+{
+    public class EventStoreRepository : IEventStoreRepository
+    {
+        private readonly IMongoCollection<EventModel> _eventStoreCollection;
+        public EventStoreRepository(IOptions<MongoDbConfig> config)
+        {
+            var mongoClient = new MongoClient(config.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(config.Value.Database);
+            _eventStoreCollection = mongoDatabase.GetCollection<EventModel>(config.Value.Collection);
+        }
+        public async Task CreateAsync(EventModel @event)
+        {
+            await _eventStoreCollection.InsertOneAsync(@event).ConfigureAwait(false);
+        }
+
+        public async Task<List<EventModel>> FindByAggregateId(Guid aggregateId)
+        {
+            return await _eventStoreCollection.Find(e => e.AggregateIdentifier == aggregateId)
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+    }
+}
