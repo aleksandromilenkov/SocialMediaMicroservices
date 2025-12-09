14ce749d9e90a48b339e86d5ab846bcebb50f216
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2025-12-09 18:00:23 +0100

    Query Handler Interface(Colleague) and Concrete Colleague and Registration of Query Handler, Query Dispatcher and all handlers in the query dispatcher

--- SocialMediaPostSolution/Post.Query/Post.Query.Api/Program.cs
+++ SocialMediaPostSolution/Post.Query/Post.Query.Api/Program.cs
@@ -1,9 +1,14 @@
 using Confluent.Kafka;
 using CQRS.Core.Consumers;
+using CQRS.Core.Infrastructure;
+using CQRS.Core.Queries;
 using Microsoft.EntityFrameworkCore;
+using Post.Query.Api.Queries;
+using Post.Query.Domain.Entities;
 using Post.Query.Domain.Repositories;
 using Post.Query.Infrastructure.Consumers;
 using Post.Query.Infrastructure.DataAccess;
+using Post.Query.Infrastructure.Dispatchers;
 using Post.Query.Infrastructure.Handlers;
 using Post.Query.Infrastructure.Repositories;
 
@@ -15,17 +20,31 @@ builder.Services.AddDbContextFactory<DatabaseContext>(options =>
 });
 builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IQueryHandler, QueryHandler>();
 builder.Services.AddScoped<IEventHandler, Post.Query.Infrastructure.Handlers.EventHandler>();
 builder.Services.Configure<ConsumerConfig>(builder.Configuration.GetSection("ConsumerConfig"));
 builder.Services.AddScoped<IEventConsumer, EventConsumer>();
 builder.Services.AddHostedService<ConsumerHostedService>();
+builder.Services.AddSingleton<IQueryDispatcher<PostEntity>, QueryDispatcher>();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-// Add services to the container.
-
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var provider = scope.ServiceProvider;
+
+    var dispatcher = provider.GetRequiredService<IQueryDispatcher<PostEntity>>();
+    var queryHandler = provider.GetRequiredService<IQueryHandler>();
+
+    dispatcher.RegisterHandler<FindAllPostsQuery>(queryHandler.HandleAsync);
+    dispatcher.RegisterHandler<FindPostByIdQuery>(queryHandler.HandleAsync);
+    dispatcher.RegisterHandler<FindPostsByAuthor>(queryHandler.HandleAsync);
+    dispatcher.RegisterHandler<FindPostsWithCommentsQuery>(queryHandler.HandleAsync);
+    dispatcher.RegisterHandler<FindPostsWithLikesQuery>(queryHandler.HandleAsync);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ SocialMediaPostSolution/Post.Query/Post.Query.Api/Queries/QueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Post.Query.Domain.Entities;
+using Post.Query.Domain.Repositories;
+
+namespace Post.Query.Api.Queries
+{
+    public class QueryHandler(IPostRepository _postRepository) : IQueryHandler
+    {
+        public async Task<List<PostEntity>> HandleAsync(FindAllPostsQuery query)
+        {
+            return await _postRepository.ListAllAsync();
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostByIdQuery query)
+        {
+            var list = new List<PostEntity>
+            {
+                await _postRepository.GetByIdAsync(query.Id)
+            };
+            return list;
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostsByAuthor query)
+        {
+            return await _postRepository.ListByAuthorAsync(query.Author);
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostsWithCommentsQuery query)
+        {
+            return await _postRepository.ListWithCommentsAsync();
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostsWithLikesQuery query)
+        {
+           return await _postRepository.ListWithLikesAsync(query.NumberOfLikes);
+        }
+    }
+}
